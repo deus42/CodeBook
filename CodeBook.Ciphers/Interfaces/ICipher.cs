37a8b0e06766f10d3b74282724a898379949fe46
Alexey Gapchenko <deusson@gmail.com>
2016-01-23 15:56:40 +0200

    Missing interface

---
+++ CodeBook.Ciphers/Interfaces/ICipher.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ICipher.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the ICipher type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CodeBook.Ciphers.Interfaces
+{
+    /// <summary>
+    /// The Cipher interface.
+    /// </summary>
+    internal interface ICipher
+    {
+        /// <summary>
+        /// Encrypt message in plaintext
+        /// </summary>
+        /// <param name="plaintext">Message in plaintext</param>
+        /// <returns>Ciphertext</returns>
+        string Encrypt(string plaintext);
+
+        /// <summary>
+        /// Decrypt ciphertext
+        /// </summary>
+        /// <param name="ciphertext">Ciphertext</param>
+        /// <returns>Plaintext</returns>
+        string Decrypt(string ciphertext);
+    }
+}
