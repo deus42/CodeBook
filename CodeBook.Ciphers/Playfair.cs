cb862796db38541418ba461d27b853ef50769eb9
Alexey Gapchenko <deusson@gmail.com>
2016-02-18 16:24:34 +0200

    Some missing files.

---
+++ CodeBook.Ciphers/Playfair.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using CodeBook.Ciphers.Interfaces;
+using CodeBook.Languages;
+
+namespace CodeBook.Ciphers
+{
+    /// <summary>
+    ///     Playfair cipher. Only English lowercase for now
+    /// </summary>
+    public class Playfair : ICipher
+    {
+        private const int AlphabetCount = 25;
+
+        /// <summary>
+        ///     Constructor. 5x5 matrix for English alphabet. Excluding 'j'.
+        /// </summary>
+        /// <param name="keyword">Keyword avoiding J (will be ignored)</param>
+        public Playfair(string keyword)
+        {
+            var key = keyword.Distinct().Where(c => c != 'j').ToList();
+            var alphabet = key.Concat(Alphabet.English26.Where(c => c != 'j').Except(key)).ToArray();
+            if (alphabet.Length != AlphabetCount)
+            {
+                throw new ArgumentException($"Alphabet should be {AlphabetCount} length!");
+            }
+
+            var matrix = new char[5][];
+            var j = 0;
+            for (var i = 0; i < alphabet.Length; i++)
+            {
+                if (i > 0 && i%5 == 0) j++;
+
+                if (matrix[j] == null)
+                {
+                    matrix[j] = new char[5];
+                }
+
+                matrix[j][i%5] = alphabet[i];
+            }
+        }
+
+        public string Encrypt(string plaintext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decrypt(string ciphertext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeBook.Tests/Languages/SpellCheckTest.cs
@@ -0,0 +1,46 @@
+﻿using CodeBook.Languages;
+using NUnit.Framework;
+
+namespace CodeBook.Tests.Languages
+{
+    /// <summary>
+    /// Spell checker tests. Please note that Spanish must be installed in your system.
+    /// </summary>
+    [TestFixture]
+    public class SpellCheckTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void TestSpellCheck_English()
+        {
+            var spellchecker = new Dictionary();
+            var result = spellchecker.SpellCheck("word");
+            Assert.IsTrue(result);
+
+            // try garbish
+            var badresult = spellchecker.SpellCheck("worrd");
+            Assert.IsFalse(badresult);
+
+
+            // try Spanish
+            badresult = spellchecker.SpellCheck("mujer");
+            Assert.IsFalse(badresult);
+        }
+
+        [Test]
+        public void TestSpellCheck_Spanish()
+        {
+            var spellchecker = new Dictionary("es");
+
+            var result = spellchecker.SpellCheck("palabra");
+            Assert.IsTrue(result);
+
+            var badresult = spellchecker.SpellCheck("word");
+            Assert.IsFalse(badresult);
+        }
+    }
+}
