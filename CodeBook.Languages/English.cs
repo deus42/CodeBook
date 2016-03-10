a895e045a3495805cdf79e6ae8016dbe1e5a3e44
Alexey Gapchenko <deusson@gmail.com>
2016-03-10 19:43:37 +0200

    Added missed new files.

---
+++ CodeBook.Languages/English.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeBook.Languages
+{
+    /// <summary>
+    /// English language.
+    /// </summary>
+    public class English : ILanguage
+    {
+        private readonly char[] alphabet;
+
+        public English(char[] alphabet = null)
+        {
+            this.alphabet = alphabet ?? Alphabet.English26;
+        }
+
+        /// <summary>
+        /// Rotates a text.
+        /// </summary>
+        /// <param name="text">Text to rotate.</param>
+        /// <param name="shift">Shift of the rotation.</param>
+        /// <returns></returns>
+        public string RotateText(string text, int shift)
+        {
+            string result = string.Empty;
+            foreach (var c in text)
+            {
+                var index = (Array.IndexOf(this.alphabet, c) + shift + this.alphabet.Length) % this.alphabet.Length;
+                result += this.alphabet[index];
+            }
+            return result;
+        }
+    }
+}
---
+++ CodeBook.Languages/ILanguage.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeBook.Languages
+{
+    /// <summary>
+    /// Represents a language
+    /// </summary>
+    public interface ILanguage
+    {
+        string RotateText(string text, int shift);
+    }
+}
